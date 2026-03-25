da1326b56f0705053eec07613661bd750a7bd285
RenzoRC99 <renzoromero99@hotmail.com>
2026-03-25 10:19:36 +0100

    first commit

---
+++ src/main/java/com/standofit/back/StandoFitApplication.java
@@ -0,0 +1,13 @@
+package com.standofit.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandoFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StandoFitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/standofit/back/StandoFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.standofit.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandoFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
